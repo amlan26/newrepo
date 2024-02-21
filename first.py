0b8217059500400ea16458e76901ec3d6aff4150
amlan26 <mdadnanamlan@gmail.com>
2024-02-21 12:19:00 +0600

    token is added

--- first.py
+++ first.py
@@ -4,3 +4,8 @@ n2 = 3.5
 sum = n1 + n2
 
 print('The  sum of {0} and {1} is {2}'.format(n1,n2, sum))
+
+
+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
+
+git remote set-url origin https://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@github.com/amlan26/newrepo
